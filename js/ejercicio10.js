6e3b02594bc21baa759371f210856a6418b89854
Tomas Quispe <tomasbquispe@gmail.com>
2023-02-28 15:29:24 -0300

    hice el ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,3 +1,10 @@
 //Tarea -10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
 
+let numero = parseInt(prompt("ingrese un numero"));
 
+if (numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0){
+    document.write("Si es divisible por 2,3,5 y 7 el numero que elegiste");
+}else{
+    document.write("No es divisible por 2, por 3, por 5, nI por 7");
+
+}
