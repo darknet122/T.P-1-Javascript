428bb6118952644b5cb818ca885d5719e5f174d4
Tomas Quispe <tomasbquispe@gmail.com>
2023-02-28 01:33:13 -0300

    hice el punto 4 de la tarea y agregue su lind

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,1 +1,3 @@
 //Tarea -4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+let nombre = prompt ("ingrese su nombre")
+document.write('Hola '+ nombre )
