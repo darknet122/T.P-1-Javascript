858cad01def8d7a4e362474c422dbe39b1ee72a3
Tomas Quispe <tomasbquispe@gmail.com>
2023-02-28 17:30:33 -0300

    arreglos al ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,9 +1,9 @@
 //Tarea -6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 let num1 = prompt('ponga un numero aqui');
-let num2 = prompt('ponga otro numero aqui')
+let num2 = prompt('ponga otro numero aqui');
 
 if (num1 > num2){
-    document.write("El mayor es "+ num1)
+    document.write("El mayor es "+ num1);
 }else {
-    document.write("El mayor es "+ num2)
+    document.write("El mayor es "+ num2);
 }
