a4d1c5c0d1830a257920ad8ea0c1645023f7cadb
Tomas Quispe <tomasbquispe@gmail.com>
2023-02-28 17:37:05 -0300

    agregue el parseInt al ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,6 +1,6 @@
 //Tarea -6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
-let num1 = prompt('ponga un numero aqui');
-let num2 = prompt('ponga otro numero aqui');
+let num1 = parseInt(prompt('ponga un numero aqui'));
+let num2 = parseInt(prompt('ponga otro numero aqui'));
 
 if (num1 > num2){
     document.write("El mayor es "+ num1);
